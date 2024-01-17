dc03e01e8d74aaaee9c86f546f0b6be7ec0989f4
Cássia <cascrisbern@gmail.com>
2024-01-17 16:55:23 -0300

    added scrollbar added task component added style for the input checkbox

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,13 +1,21 @@
 import styles from "./List.module.css"
 import clipboard from "../../assets/clipboard.svg"
 
+import { Task } from "../Task"
+
 export function List() {
 	return (
 		<div className={styles.list}>
-			<div className={styles.empty}>
+			{/* <div className={styles.empty}>
 				<img src={clipboard} alt="icon of a clipboard" />
 				<p>You don't have any tasks registered yet</p>
 				<p>Create tasks and organize your todo items</p>
+			</div> */}
+
+			<div className="task">
+				<Task />
+				<Task />
+				<Task />
 			</div>
 		</div>
 	)
--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,11 +1,16 @@
 import styles from "./Task.module.css"
 
+import { Trash } from "phosphor-react"
+
 export function Task() {
 	return (
 		<div className={styles.task}>
-			<label>
-				<input type="checkbox" />
-			</label>
+			<input type="checkbox" />
+			<p>
+				Integer urna interdum massa libero auctor neque turpis turpis semper.
+				Duis vel sed fames integer.
+			</p>
+			<Trash />
 		</div>
 	)
 }
