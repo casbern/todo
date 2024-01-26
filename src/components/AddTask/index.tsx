faa1baf7da860f09f9ba90dccfa4189b6242478c
Cássia <cascrisbern@gmail.com>
2024-01-26 20:23:15 -0300

    added funcionality to add tasks

--- src/components/AddTask/index.tsx
+++ src/components/AddTask/index.tsx
@@ -1,12 +1,33 @@
 import styles from "./AddTask.module.css"
 
 import { PlusCircle } from "phosphor-react"
+import { ChangeEvent, useState } from "react"
 
-export function AddTask() {
+export function AddTask({ onAddTask }) {
+	const [addTask, setAddTask] = useState("")
+
+	function handleInputValue(e: ChangeEvent<HTMLInputElement>) {
+		setAddTask(e.target.value)
+	}
+
+	function handleAddTask() {
+		if (addTask === "") {
+			console.log("Por favor, adicione uma tarefa.")
+		} else {
+			console.log(addTask)
+			onAddTask(addTask)
+			setAddTask("")
+		}
+	}
+
 	return (
 		<div className={styles.addTask}>
-			<input type="text" placeholder="Add a new task" />
-			<button>
+			<input
+				type="text"
+				placeholder="Add a new task"
+				onChange={handleInputValue}
+			/>
+			<button onClick={handleAddTask}>
 				Add
 				<PlusCircle />
 			</button>
