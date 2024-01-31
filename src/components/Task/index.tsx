6ccc9e3cd6e7b1c0ca5121b4e695378ee772ad85
Cássia <cascrisbern@gmail.com>
2024-01-31 19:28:42 +0200

    added feature for when the task is marked as completed.

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,15 +1,22 @@
 import styles from "./Task.module.css"
 
 import { Trash } from "phosphor-react"
+import { useState } from "react"
 
 interface Props {
 	task: string
 }
 
 export function Task({ task }: Props) {
+	const [isChecked, setIsChecked] = useState(false)
+
+	function handleCheckboxChange() {
+		setIsChecked(!isChecked)
+	}
+
 	return (
-		<div className={styles.task}>
-			<input type="checkbox" />
+		<div className={`${styles.task} ${isChecked ? styles.checked : ""}`}>
+			<input type="checkbox" onChange={handleCheckboxChange} />
 			<p>{task}</p>
 			<button>
 				<Trash />
